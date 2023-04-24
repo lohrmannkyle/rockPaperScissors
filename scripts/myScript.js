7421a99e83e9f506257f1ae26901dc4d81f0e059
kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-04-23 19:18:40 -0700

    Finish GUI

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,50 +1,69 @@
+let playerWins = 0;
+let computerWins = 0;
 
-function game(){
-    function getComputerChoice(){
-        choices = ['rock', 'paper', 'scissors']
-        return choices[Math.round(Math.random()*2)]
-    }
-    
-    function getPlayerChoice(){
-        return prompt("Please enter 'rock', 'paper' or 'scissors'.").toLowerCase()
-        Enumerator
-    }
 
-    function getWinner(pChoice, cChoice){
-        if (pChoice === 'rock' && cChoice === 'scissors'
-        || pChoice === 'paper' && cChoice === 'rock'
-        || pChoice === 'scissors' && cChoice === 'paper'){
-            return 1
-        } 
-        else if (pChoice === cChoice) {
-            return 0
-        } else {
-            return 2
-        }
-        
-    }   
-    
-    let playerWins = 0
-    let computerWins = 0
-
-    for (let i = 0; i < 5; i++){
-        const player = getPlayerChoice()
-        const computer = getComputerChoice()
-
-        let winner = getWinner(player, computer)
-
-        if (winner === 1){
-            playerWins++
-            console.log(`You win, ${player} beats ${computer}!`)
-        } else if (winner === 2) {
-            computerWins++
-            console.log(`You lose, ${computer} beats ${player}!`)
-        } else {
-            console.log('Tie!')
-        }
-    }
-    console.log(`Score\nComputer: ${computerWins}\nPlayer: ${playerWins}`)
+function getComputerChoice(){
+    choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.round(Math.random()*2)];
 }
 
-game()
+function getWinner(pChoice, cChoice){
+    if (pChoice === 'rock' && cChoice === 'scissors'
+    || pChoice === 'paper' && cChoice === 'rock'
+    || pChoice === 'scissors' && cChoice === 'paper'){
+        return 1;
+    } 
+    else if (pChoice === cChoice) {
+        return 0;
+    } else {
+        return 2;
+    }
+    
+}   
+
+function displayWInner(){
+    const parent = document.querySelector('.score');
+    const score = document.createElement('p');
+    score.textContent = 
+    `Score
+    Computer: ${computerWins}
+    Player: ${playerWins}`;
+
+    parent.appendChild(score);
+}
+function removeButtons(){
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.style.display="none");
+}
+
+function playRound(player){
+    const computer = getComputerChoice();
+
+    let winner = getWinner(player, computer);
+    
+    if (winner === 1){
+        playerWins++;
+        console.log(`You win, ${player} beats ${computer}!`);
+    } else if (winner === 2) {
+        computerWins++;
+        console.log(`You lose, ${computer} beats ${player}!`);
+    } else {
+        console.log('Tie!');
+    }
+    if (playerWins === 5 || computerWins === 5) {
+        displayWInner();
+        removeButtons();
+    }
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click',() => {
+    const player = button.classList.value;
+    console.log(player);
+    playRound(player);
+}))
+
+
+
 
